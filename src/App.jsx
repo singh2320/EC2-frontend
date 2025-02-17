28f934f7a267d7cbf620e387ca2ff9615af7af7b
singh2320 <122520100+singh2320@users.noreply.github.com>
2025-02-17 15:02:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+      <h1>WELCOME TO EC2 RUNNING INSTANCE OF RUDRAKSHI..</h1>
+      <h3>LAUNCH !!! ......</h3>
+    </div>
+  );
+}
+
+export default App;
